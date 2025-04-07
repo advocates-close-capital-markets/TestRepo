593628e33aa446c200c28cd9c6c3d21c9a1bebf3
Advocates Close <javaad.ali@advocatesclose.nyc>
2025-04-07 15:40:45 -0500

    Initial Commit

---
+++ TestRepoTests/TestRepoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoTests.swift
+//  TestRepoTests
+//
+//  Created by AdvocatesClose on 4/7/25.
+//
+
+import Testing
+@testable import TestRepo
+
+struct TestRepoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
